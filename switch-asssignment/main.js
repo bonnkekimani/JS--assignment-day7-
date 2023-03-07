b726ffef91024d9a1fb6a8b9f0a065a3a2b0f510
Daniel Karanja <sirkaranja12@gmail.com>
2023-03-07 14:35:37 +0300

    done

---
+++ switch-asssignment/main.js
@@ -0,0 +1,24 @@
+//displaying on console
+//  const day = prompt('Enter a day(either Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday):');
+// switch (day){
+//     case 'monday':
+//     case 'tuesday':
+//     case 'wednesday':
+//     case 'thursday':
+//     case 'friday':
+//         console.log(`${day} is a weekday`);
+//         break;
+//     case 'saturday':
+//     case 'sunday':
+//         console.log(`${day} is a weekend`);
+//         break;
+//     default:
+//         console.log("Invalid Input")
+//         break;
+    
+        
+        
+// }
+
+// //Displaying in the browser
+document.getElementById("demo").innerHTML="Today is"+' '+ day;
